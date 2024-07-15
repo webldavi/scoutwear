678e646efcae59b0281b50cf10bbadb9524d73ba
Luis Davi <luisdavi.contato@gmail.com>
2024-07-15 16:01:43 -0300

    update contribuitors section

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
   <NuxtLayout is-home>
     <section class="w-full min-h-screen bg-scroll relative overflow-hidden rounded-b-2xl" id="heroSection">
       <div class="w-full h-full absolute top-0 bg-brand/80 pt-[210px] flex items-center justify-center text-white/80 flex flex-col gap-4">
-        <h1 class="text-9xl font-bold">SCOUTWEAR</h1>
+        <h1 class="text-6xl lg:text-9xl font-bold ">SCOUTWEAR</h1>
 
-        <p class="text-3xl max-w-[750px] text-center">Vista-se para a aventura diária com estilo e conforto. Inspire-se no espírito escoteiro.</p>
+        <p class="text-xl lg:text-3xl max-w-[750px] text-center">Vista-se para a aventura diária com estilo e conforto. Inspire-se no espírito escoteiro.</p>
 
-        <span class="text-2xl">@scoutwear.co</span>
+        <span class="text-xl lg:text-2xl">@scoutwear.co</span>
       </div>
     </section>
 
@@ -22,6 +22,6 @@
   background-image: url('~/assets/images/background_hero_section.jpeg');
   background-repeat: no-repeat;
   background-size: cover;
-  background-position-y: -600px;
+  background-position: center;
 }
 </style>
